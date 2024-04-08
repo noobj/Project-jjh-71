bf61c2a632b660506d4a1a73ad5301d3ca3cb725
noobj <jingfu99@gmail.com>
2024-04-08 15:43:49 -0700

    feat: add open lib api

--- app/Http/Controllers/Admin/AdminBookController.php
+++ app/Http/Controllers/Admin/AdminBookController.php
@@ -10,26 +10,46 @@ use Illuminate\Support\Facades\Auth;
 class AdminBookController extends Controller
 {
     public function addBook(Request $request) {
-        // TODO: validation input
+        Book::validate($request);
+        $client = new \GuzzleHttp\Client();
+        $url = "https://openlibrary.org/search.json?q=";
+        $title = str_replace(' ', '+', $request->post('name'));
+        $url .= $title;
+        $response = $client->request('GET', $url);
+        $body = json_decode($response->getBody()->getContents(), true); // '{"id": 1420053, "name": "guzzle", ...}'
+        $author = $body['docs'][0]['author_name'][0];
+        $isbn = $request->post('isbn');
+        $isbn_url ="https://www.googleapis.com/books/v1/volumes?q=isbn:".$isbn;
+        $isbn_response = $client->request('GET',$isbn_url);
+        $body_isbn = json_decode($isbn_response->getBody()->getContents(),true);
+        $name = $body_isbn['items'][0]['volumeInfo']['title'];
 
-
-        // TODO: fetch author name from api
-        return Book::create([
-            'title' => $request->post('name'),
+        Book::create([
+            'title' => $name,
             'description' => $request->post('description'),
             'isbn' => $request->post('isbn'),
-            'author' => 'Fetched from API',
+            'author' => $author,
             'quantity' => $request->post('quantity'),
             'category_id' => $request->post('category'),
         ]);
+
+        return redirect()->route('home.admin.index');
     }
 
     public function manageBook(Request $request, $id) {  //switch case "action" & update or delete
         if ($request->input('action') == 'update') {
             Book::validate($request);
+
+            $book = Book::findOrFail($id);
+            $book->setName($request->input('name'));
+            $book->setCategory($request->input('category'));
+            $book->setDesc($request->input('description'));
+            $book->setQuantity($request->input('quantity'));
+            $book->setIsbn($request->input('isbn'));
+            $book->save();
         }
         else if ($request->input('action') == 'delete') {
-
+            Book::destroy($id);
         }
         return redirect()->route('home.admin.index');
     }
